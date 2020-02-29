eba73437244177c8ebd9ca541c9f33f6f60670c2
Sebastián Paiva <sebastian.paivam@gmail.com>
2020-02-29 00:39:59 -0300

    fixes

--- projects/ngx-crud/src/public-api.ts
+++ projects/ngx-crud/src/public-api.ts
@@ -3,6 +3,8 @@
  */
 
 export * from './lib/ngx-crud.component';
+export * from './lib/ngx-crud-form/ngx-crud-form.component';
+export * from './lib/ngx-crud-import/ngx-crud-import.component';
 export * from './lib/ngx-crud.module';
 export * from './lib/crud.interface';
 export * from './lib/functions/params';
